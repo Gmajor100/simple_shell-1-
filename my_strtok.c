9c48d5f1bb86d64de57bb27048710ff4b1beccf7
Ammrtranquility <okonemmanuelasuquo1@gmail.com>
2023-04-26 15:05:19 -0700

    my_strtok.c

---
+++ my_strtok.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * my_strtok - tokenizes a string
+ * @str: the string to tokenize
+ * @delim: the delimiter to use
+ *
+ * Return: a pointer to the next token or NULL if there are no more tokens
+ */
+char *my_strtok(char *str, const char *delim)
+{
+	static char *joshua;
+	char *moda;
+
+	if (str == NULL && joshua == NULL)
+	{
+		return (NULL);
+	}
+
+	if (str != NULL)
+	{
+		joshua = str;
+	}
+	modern = joshua;
+
+	while (*joshua != '\0')
+	{
+		if (_strchr(delim, *joshua) != NULL)
+		{
+			*joshua = '\0';
+			joshua++;
+			return (modern);
+		}
+		joshua++;
+	}
+
+	if (*modern != '\0')
+	{
+		return (modern);
+	}
+	joshua = NULL;
+	return (NULL);
+}
+
