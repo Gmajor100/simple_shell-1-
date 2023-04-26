1fd26f3895dc30bfd6ef665e6a8a1170e5e0ffc9
Ammrtranquility <okonemmanuelasuquo1@gmail.com>
2023-04-26 15:18:56 -0700

    string1.c

---
+++ string1.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies strings.
+ * @dest: destination.
+ * @src: source string.
+ *
+ * Return: a pointer to dest.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	char *tm = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (lf);
+}
+
+/**
+ * _strcat - concatenate two string.
+ * @dest: destination.
+ * @src: source string.
+ *
+ * Return: a pointer to dest.
+ */
+char *_strcat(char *dest, const char *src)
+{
+	char *lf = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (lf);
+}
+
